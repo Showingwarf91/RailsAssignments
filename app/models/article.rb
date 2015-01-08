95e1593f19d3136b2a0ac9b486017e320a45aeb0
showingWarf91 <joshi.anujo7@gmail.com>
2015-01-08 17:05:57 +0545

    Question5: scope(article_of_yesterday_desc) added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_and_belongs_to_many :tags
+  scope :article_of_yesterday_desc, -> {where(created_at: (Time.now.midnight - 1.day)..Time.now.midnight).order(created_at: :desc)}
 end
+
