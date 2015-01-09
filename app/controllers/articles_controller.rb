c32647a4176a67dde312ab56f0af57d027770980
showingWarf91 <joshi.anujo7@gmail.com>
2015-01-09 17:12:20 +0545

    Question3: Associated Articles with tag View and Controller added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,8 @@
 class ArticlesController < ApplicationController
 
   def display_article
-    p "ASdsadsa"
-    @linked_article = Article.find_by_title(params[:query])
+    @linked_article = Article.find(params[:query])
     p @linked_article
   end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
       get 'display_article'
     end
   end
+  resource :tags do
+    member do
+      get 'display_article_related_to_tags'
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
